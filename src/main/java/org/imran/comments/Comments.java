bccbfbe996663e5642f1efb47223886eccf9cdd3
imran <chenamihab@gmail.com>
2023-11-15 02:05:50 +0600

    add comment's details

---
+++ src/main/java/org/imran/comments/Comments.java
@@ -0,0 +1,26 @@
+package org.imran.comments;
+
+/***
+ * Comments are ignored by compile. Comments are used for describing the code or explaining the code samples.
+ * <p>
+ * In Java, there are three types of comments available.
+ * 1. Single-line comment
+ * 2. Multi-line comment
+ * 3. Documentation comment
+ * You reading this text within the documentation comment.
+ * Single-line comment's syntax is "// This is a single-line comment".
+ * Multi-line example shown bellow
+ */
+public class Comments {
+
+    // Single-line comment example
+
+    /*
+    Multi-line
+    comment example
+     */
+    public static void main(String[] args) {
+        System.out.println("Comment example.");
+    }
+
+}
--- src/main/java/org/imran/helloworld/HelloWorld.java
+++ src/main/java/org/imran/helloworld/HelloWorld.java
@@ -1,13 +1,19 @@
 package org.imran.helloworld;
 
 /***
- * Create a file named "HelloWorld.class" and within this file, create a public class named "HelloWorld"
+ * Create a file named "HelloWorld.java" and within this file, create a public class named "HelloWorld"
  * In Java Programming Language you need to add "class" keyword for creating a class.
+ * <p>
+ * Every line of code that runs in Java must be inside a class. In our example, we named the class HelloWorld.
+ * A class should always start with an uppercase first letter. Java is case-sensitive.
+ * The name of the java file must match the class name.
  */
 public class HelloWorld {
 
-    // Add a public static method "main". This is the entry point of a Java Program.
+    // Add a public static method "main()". This is the entry point of a Java Program.
+    // Any code inside the main() method will be executed.
     // When you run this program it will print "Hello World".
+    // We use println() for printing text on the screen.
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
