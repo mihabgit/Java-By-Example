10cfa064579c4766c5628e2666458d45c1a59d65
imran <chenamihab@gmail.com>
2023-11-15 01:37:49 +0600

    initial commit

---
+++ src/main/java/org/imran/helloworld/HelloWorld.java
@@ -0,0 +1,15 @@
+package org.imran.helloworld;
+
+/***
+ * Create a file named "HelloWorld.class" and within this file, create a public class named "HelloWorld"
+ * In Java Programming Language you need to add "class" keyword for creating a class.
+ */
+public class HelloWorld {
+
+    // Add a public static method "main". This is the entry point of a Java Program.
+    // When you run this program it will print "Hello World".
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
